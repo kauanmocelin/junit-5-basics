c990d389d9474543d70275c3a09c66dbae932ec9
kauanmocelin <kauanmocelin@gmail.com>
2021-11-08 15:29:48 -0300

    test: add test name description with tag

--- src/test/java/dev/kauanmocelin/MathUtilsTest.java
+++ src/test/java/dev/kauanmocelin/MathUtilsTest.java
@@ -1,9 +1,6 @@
 package dev.kauanmocelin;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -28,18 +25,21 @@ class MathUtilsTest {
     }
 
     @Test
-    void shouldReturnSumFromTwoNumbersWithSuccessful() {
+    @DisplayName("Should sum two numbers with successful")
+    void shouldSumTwoNumbersWithSuccessful() {
         final int result = mathUtils.sum(2, 3);
         assertThat(result).isEqualTo(5);
     }
 
     @Test
+    @DisplayName("Should return ArithmeticException when divide by zero")
     void shouldReturnArithmeticExceptionWhenDivideByZero() {
         assertThatThrownBy(() -> mathUtils.divide(1, 0))
                 .isInstanceOf(ArithmeticException.class);
     }
 
     @Test
+    @DisplayName("should return right circle area when successful")
     void shouldReturnRightCircleAreaWhenSuccessful() {
         assertThat(mathUtils.calculateCircleArea(10)).isEqualTo(314.1592653589793);
     }
