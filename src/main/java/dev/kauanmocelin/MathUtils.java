147802f1accabacc542b7ed68179a58f95fde561
kauanmocelin <kauanmocelin@gmail.com>
2021-11-08 10:13:37 -0300

    feat: add other basic calculator operations

--- src/main/java/dev/kauanmocelin/MathUtils.java
+++ src/main/java/dev/kauanmocelin/MathUtils.java
@@ -4,4 +4,20 @@ public class MathUtils {
     public Double sum(double parameterA, double parameterB) {
         return parameterA + parameterB;
     }
+
+    public Double subtract(double parameterA, double parameterB) {
+        return parameterA - parameterB;
+    }
+
+    public Double multiply(double parameterA, double parameterB) {
+        return parameterA * parameterB;
+    }
+
+    public Double divide(double parameterA, double parameterB) {
+        return parameterA / parameterB;
+    }
+
+    public Double calculateCircleArea(double radius) {
+        return Math.PI * radius * radius;
+    }
 }
